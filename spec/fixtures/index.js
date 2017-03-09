787402e37a95d0e1955861e14d71b70ecdd781f8
Daniel St. Jules <danielst.jules@gmail.com>
2017-03-09 16:19:00 -0500

    Add dirmap for fixture loading

--- spec/fixtures/index.js
+++ spec/fixtures/index.js
@@ -1,12 +1,4 @@
-var path = require('path');
+var path   = require('path');
+var dirmap = require('dirmap');
 
-var absolutePaths = {};
-var fixtures = ['intersection', 'redundantIntersection', 'indentation',
-  'identifiers', 'es6Module', 'commonjs', 'amd', 'simple', 'smallDiffs',
-  'matches'];
-
-fixtures.forEach(function(fixture) {
-  absolutePaths[fixture] = path.resolve(__dirname, fixture + '.js');
-});
-
-module.exports = absolutePaths;
+module.exports = dirmap(path.resolve(__dirname), true);
