222af9b86b9821790637e6fccee04e88e3951dc1
Daniel St. Jules <danielst.jules@gmail.com>
2017-03-27 16:07:52 -0400

    s/nodeUtils/NodeUtils since making it an ES6 class with static methods

--- spec/nodeUtilsSpec.js
+++ spec/nodeUtilsSpec.js
@@ -1,15 +1,15 @@
 var expect    = require('expect.js');
 var fixtures  = require('./fixtures');
 var helpers   = require('./helpers');
-var nodeUtils = require('../lib/nodeUtils');
+var NodeUtils = require('../lib/NodeUtils');
 
-describe('nodeUtils', function() {
+describe('NodeUtils', function() {
   describe('getChildren', function() {
     it('ignores null children', function() {
       var node = helpers.parse(fixtures.nullChildren)[1].expression.left;
       // node.elements is an array with a leading null that should be ignored,
       // followed by an identifier
-      var res = nodeUtils.getChildren(node);
+      var res = NodeUtils.getChildren(node);
       expect(res).to.have.length(1);
       expect(res[0].type).to.be('Identifier');
     });
@@ -19,18 +19,18 @@ describe('nodeUtils', function() {
     it('returns true for an import declaration', function() {
       // ImportDeclaration
       var nodes = [helpers.parse(fixtures.es6Module)[0]];
-      expect(nodeUtils.isES6ModuleImport(nodes)).to.be(true);
+      expect(NodeUtils.isES6ModuleImport(nodes)).to.be(true);
     });
 
     it('returns false for export declaration', function() {
       // ExportNamedDeclaration
       var nodes = [helpers.parse(fixtures.es6Module)[1]];
-      expect(nodeUtils.isES6ModuleImport(nodes)).to.be(false);
+      expect(NodeUtils.isES6ModuleImport(nodes)).to.be(false);
     });
 
     it('returns false otherwise', function() {
       var nodes = helpers.parse(fixtures.commonjs);
-      expect(nodeUtils.isES6ModuleImport(nodes)).to.be(false);
+      expect(NodeUtils.isES6ModuleImport(nodes)).to.be(false);
     });
   });
 
@@ -38,28 +38,28 @@ describe('nodeUtils', function() {
     it('returns true for an expression containing a define', function() {
       // First expression is define
       var nodes = [helpers.parse(fixtures.amd)[0]];
-      expect(nodeUtils.isAMD(nodes)).to.be(true);
+      expect(NodeUtils.isAMD(nodes)).to.be(true);
     });
 
     it('returns true for an expression containing a define', function() {
       // Third expression is require
       var nodes = [helpers.parse(fixtures.amd)[2]];
-      expect(nodeUtils.isAMD(nodes)).to.be(true);
+      expect(NodeUtils.isAMD(nodes)).to.be(true);
     });
 
     it('returns true even if the function is a property', function() {
       var nodes = [helpers.parse(fixtures.amd)[4]];
-      expect(nodeUtils.isAMD(nodes)).to.be(true);
+      expect(NodeUtils.isAMD(nodes)).to.be(true);
     });
 
     it('returns true even if a nested property', function() {
       var nodes = [helpers.parse(fixtures.amd)[6]];
-      expect(nodeUtils.isAMD(nodes)).to.be(true);
+      expect(NodeUtils.isAMD(nodes)).to.be(true);
     });
 
     it('returns false otherwise', function() {
       var nodes = helpers.parse(fixtures.commonjs);
-      expect(nodeUtils.isAMD(nodes)).to.be(false);
+      expect(NodeUtils.isAMD(nodes)).to.be(false);
     });
   });
 
@@ -67,18 +67,18 @@ describe('nodeUtils', function() {
     it('returns true for an expression containing a require', function() {
       // First node is an ExpressionStatement
       var nodes = [helpers.parse(fixtures.commonjs)[0]];
-      expect(nodeUtils.isCommonJS(nodes)).to.be(true);
+      expect(NodeUtils.isCommonJS(nodes)).to.be(true);
     });
 
     it('returns true for a declaration containing a require', function() {
       // Second node is a VariableDeclaration
       var nodes = [helpers.parse(fixtures.commonjs)[1]];
-      expect(nodeUtils.isCommonJS(nodes)).to.be(true);
+      expect(NodeUtils.isCommonJS(nodes)).to.be(true);
     });
 
     it('returns false otherwise', function() {
       var nodes = helpers.parse(fixtures.amd);
-      expect(nodeUtils.isCommonJS(nodes)).to.be(false);
+      expect(NodeUtils.isCommonJS(nodes)).to.be(false);
     });
   });
 });
