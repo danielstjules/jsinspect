9c21549ac3aed3398024ec6bec2052d879a7e494
Daniel St. Jules <danielst.jules@gmail.com>
2017-03-29 13:49:53 -0400

    Improve errors on parsing failure

---
+++ spec/parserSpec.js
@@ -0,0 +1,24 @@
+var expect  = require('expect.js');
+var helpers = require('./helpers');
+var Parser  = require('../lib/parser');
+
+describe('parse', function() {
+  describe('on error', function() {
+    var src = '[_, = [1, 2, 3];';
+    var filePath = 'broken.js';
+
+    it('includes the filename of the file that failed to parse', function() {
+      var fn = () => Parser.parse(src, filePath);
+      expect(fn).to.throwException((err) => {
+        expect(err.message).to.contain(filePath);
+      });
+    });
+
+    it('includes a caret pointing to the unexpected token', function() {
+      var fn = () => Parser.parse(src, filePath);
+      expect(fn).to.throwException((err) => {
+        expect(err.message).to.contain(`${src}\n    ^`);
+      });
+    });
+  });
+});
